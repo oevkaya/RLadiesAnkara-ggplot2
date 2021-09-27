edd1033241b629b9165ffdf4d81d41c567d73f06
Ozan Evkaya <68122167+oevkaya@users.noreply.github.com>
2021-09-27 15:10:35 +0300

    Update theme_update.R

--- R-scripts/theme_update.R
+++ R-scripts/theme_update.R
@@ -14,34 +14,4 @@ new_theme <- function(base_size = 14, base_family = "", base_line_size = base_si
           legend.key = element_rect(fill = "white", colour = NA), complete = TRUE)
 }
 
-new_theme2 <- function() {
-theme_ipsum(
-  base_family = "Arial Narrow",
-  base_size = 11.5,
-  plot_title_family = base_family,
-  plot_title_size = 18,
-  plot_title_face = "bold",
-  plot_title_margin = 10,
-  subtitle_family = base_family,
-  subtitle_size = 12,
-  subtitle_face = "plain",
-  subtitle_margin = 15,
-  strip_text_family = base_family,
-  strip_text_size = 12,
-  strip_text_face = "plain",
-  caption_family = base_family,
-  caption_size = 9,
-  caption_face = "italic",
-  caption_margin = 10,
-  axis_text_size = base_size,
-  axis_title_family = subtitle_family,
-  axis_title_size = 9,
-  axis_title_face = "plain",
-  axis_title_just = "rt",
-  plot_margin = margin(30, 30, 30, 30),
-  grid_col = "#cccccc",
-  grid = TRUE,
-  axis_col = "#cccccc",
-  axis = FALSE,
-  ticks = FALSE
-) }
+  
